00c5d1b0e91cb16e1f2b477ddd22dd6001a0ce00
FlameFrenchy <Petitchat5345@outlook.fr>
2025-04-18 19:30:28 +0200

    Create Manager.lua

---
+++ Manager.lua
@@ -0,0 +1,9 @@
+local import = require(script.Parent.Utils.Import)
+
+local mananger = {}
+
+mananger.importrepo = function(GIT_REPO_OWNER :string, GIT_REPO_NAME :string)
+	import.GET_REPO(GIT_REPO_OWNER,GIT_REPO_NAME)
+end
+
+return mananger
